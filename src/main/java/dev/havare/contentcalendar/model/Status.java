23eb7af3ed4a1aeda9634080b6b870be38f28297
ercanhavare <ercanhavare@gmail.com>
2023-04-02 18:00:17 +0300

    first commit

---
+++ src/main/java/dev/havare/contentcalendar/model/Status.java
@@ -0,0 +1,8 @@
+package dev.havare.contentcalendar.model;
+
+public enum Status {
+    IDEA,
+    IN_PROGRESS,
+    COMPLETED,
+    PUBLISHED
+}
---
+++ src/main/java/dev/havare/contentcalendar/model/Type.java
@@ -0,0 +1,8 @@
+package dev.havare.contentcalendar.model;
+
+public enum Type {
+    ARTICLE,
+    VIDEO,
+    COURSE,
+    CONFERENCE_TALK
+}
